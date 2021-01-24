9747691a60841e1feb372a8e3eba4628d33bfd24
lalithu <lalithuriti@gmail.com>
2021-01-25 03:05:01 +0530

    setup.py Details

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ setup(
     author='Lalith U.',
     author_email='lalithuriti@gmail.com',
     description='Orbital Mechanics Data Analysis Package',
+    url="https://github.com/lalithu/EllipsePy",
     packages=find_packages(),
     install_requires=['numpy', 'plotly', 'scipy'],
-    keywords=['python', 'Orbital Mechanics', 'EllipsePy'],
+    keywords=['python', 'Orbital Mechanics', 'Astrodynamics'],
     classifiers=[
-        "Intended Audience :: Education",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research"
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Astronomy"
     ]
 )
