16f23bf9990ee208a14f4df1af35852a0773b6e2
lalithu <lalithuriti@gmail.com>
2021-01-29 21:02:24 +0530

    v1.1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='EllipsePy',
-    version='0.1.2',
+    version='0.1.3',
     author='Lalith Uriti',
     author_email='lalithuriti@gmail.com',
     description='Orbital Mechanics Data Visualization and Analysis',
