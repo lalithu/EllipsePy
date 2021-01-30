aa1784ddb10101140108b43c6f7f10ee5444c53e
lalithu <lalithuriti@gmail.com>
2021-01-30 09:42:58 +0530

    v2.1.0

--- EllipsePy/__init__.py
+++ EllipsePy/__init__.py
@@ -1,6 +1,8 @@
 from .plotting import Plotter
-from .utilities import Spheres, Orbit
+from .utilities import Orbit
 from .bodies import Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+
+from .examples import *
 
 # EllisePy | Orbital Mechanics Data Visualization and Analysis
 # By Lalith Uriti, 2021
